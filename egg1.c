e74670f615f29a75150fd9e913327d35d36614dc
cwxin <l16031132@mail.ncku.edu.tw>
2015-03-15 16:47:53 +0800

    增加註解

--- egg1.c
+++ egg1.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-	int i;
-	for(i=0;i<9;++i)
+	int i;                           //宣告 i 為整數 
+	for(i=0;i<9;++i)                //為一迴圈 i =0,2, ...,8
 	{
-		printf("egg-%d\n", i+1);
+		printf("egg-%d\n", i+1);  //print egg-1, egg-2, egg-3, ...,egg-9
 	}
 }
