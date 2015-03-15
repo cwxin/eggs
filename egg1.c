16733e21b649000a0a8ae81524f5b67f883ecc82
cwxin <l16031132@mail.ncku.edu.tw>
2015-03-15 16:58:45 +0800

    稍微修改

--- egg1.c
+++ egg1.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-	int i;                           //宣告 i 為整數 
-	for(i=0;i<9;++i)                //為一迴圈 i =0,2, ...,8
+	int i;                           //i proclaimed is an integer 
+	for(i=0;i<9;++i)                //i=1,2, ...,8;it is a loop
 	{
 		printf("egg-%d\n", i+1);  //print egg-1, egg-2, egg-3, ...,egg-9
 	}
