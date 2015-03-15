54c346fa920291721222de5f778b3ea0fe72a724
cwxin <l16031132@mail.ncku.edu.tw>
2015-03-15 17:01:42 +0800

    修正錯誤

--- egg1.c
+++ egg1.c
@@ -3,7 +3,7 @@
 int main()
 {
 	int i;                           //i proclaimed is an integer 
-	for(i=0;i<9;++i)                //i=1,2, ...,8;it is a loop
+	for(i=0;i<9;++i)                //i=0,1, ...,8;it is a loop
 	{
 		printf("egg-%d\n", i+1);  //print egg-1, egg-2, egg-3, ...,egg-9
 	}
